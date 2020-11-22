e615eaee336d5546eaaa12ffb59ccae3c1545e5b
Rzyaji <74767731+Rzyaji@users.noreply.github.com>
2020-11-22 09:00:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
+const BotName = 'Rdbot'; // Nama Bot Whatsapp
+const instagramlu = '@radya ajisa'; // Nama Instagramlu cok
+const whatsapplu = 'wa.me/6282147334265'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
 const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
 const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
